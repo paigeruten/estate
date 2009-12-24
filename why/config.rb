2a637d3776dee8fc3606760d3493799493fcf086
Jeremy Ruten <jeremy.ruten@gmail.com>
2009-12-24 01:36:07 -0600

    added _why's estate source files

---
+++ why/config.rb
@@ -0,0 +1,4 @@
+TITLE = '_why\'s Estate'
+
+DESCRIPTION = 'Hey, welcome to my collection of <a href="http://wikipedia.org/wiki/Why_the_lucky_stiff">why the lucky stiff</a> links.  Everything _why has published on the internet should be accessible from here.  It works sort of like a museum that sells maps.  Many of his abandoned writings are mirrored locally here, and everything else is through external links.'
+
---
+++ why/index.rb
@@ -0,0 +1,263 @@
+INDEX_HEADER = ''
+
+LIST = [
+  ['Code', [
+    ['desc', 'I believe <a href="http://whymirror.github.com/">whymirror.github.com</a> has it all taken care of.  Still, let\'s replicate the list of projects here, for convenience and effect.'],
+    ['quiet'],
+
+    ['http://github.com/mental/bloopsaphone', 'bloopsaphone'],
+    ['http://github.com/bterlson/blimlimb', 'blimlimb'],
+    ['http://github.com/bruce/bumpspark', 'bumpspark'],
+    ['http://github.com/camping/camping', 'camping'],
+    ['http://github.com/drnic/8cpj', '8cpj'],
+    ['http://github.com/steveklabnik/hacketyhack', 'hacketyhack'],
+    ['http://github.com/hobix/hobix', 'hobix'],
+    ['http://github.com/robbyrussell/hoodwinkd', 'hoodwinkd'],
+    ['http://github.com/ecarnevale/hpricot', 'hpricot'],
+    ['http://github.com/joho/markaby', 'markaby'],
+    ['http://github.com/dannytatom/metaid', 'metaid'],
+    ['http://github.com/rkh/mixico', 'mixico'],
+    ['http://github.com/evaryont/mousehole', 'mousehole'],
+    ['http://github.com/mattjamieson/parkplace', 'parkplace'],
+    ['http://github.com/fogus/potion', 'potion'],
+    ['http://github.com/jgarber/redcloth', 'redcloth'],
+    ['http://github.com/wmorgan/sheila', 'sheila'],
+    ['http://github.com/Sophrinix/sandbox', 'sandbox'],
+    ['http://github.com/Sophrinix/TryRuby', 'Try Ruby'],
+    ['http://github.com/shoes/shoes', 'shoes'],
+    ['http://github.com/indeyets/syck', 'syck'],
+    ['http://github.com/devyn/unholy', 'unholy'],
+    ['http://github.com/fogus/chirrup', 'chirrup'],
+    ['http://github.com/fogus/skistrap', 'skistrap'],
+    ['http://github.com/fogus/fakeplus', 'fakeplus'],
+    ['http://github.com/fogus/processor', 'processor'],
+    ['http://github.com/fogus/rb_parse_args', 'rb_parse_args'],
+    ['http://github.com/whymirror/yown', 'yown'],
+    ['http://rubyforge.org/projects/dns-zone', 'dns-zone']
+  ]],
+  ['Writings', [
+    ['Books', [
+      ['http://mislav.uniqpath.com/poignant-guide/', 'Why\'s (Poignant) Guide to Ruby [HTML]'],
+      ['http://www.rubyinside.com/media/poignant-guide.pdf', 'Why\'s (Poignant) Guide to Ruby [PDF]'],
+      ['http://cloud.github.com/downloads/shoes/shoes/nks.pdf', 'Nobody Knows Shoes [PDF]']
+    ]],
+    ['Stories', [
+      ['estate://stories/theBobbyWolves.html', 'The Bobby Wolves'],
+      ['estate://stories/stuntRunner.html', 'Stunt Runner'],
+      ['estate://stories/theColorsHaveLeft.html', 'The Colors Have Left']
+    ]],
+    ['Incidents', [
+      ['estate://incidents/01.TheMilkmanWhoCouldntSleep.html', '1. The Milkman Who Couldn\'t Sleep'],
+      ['estate://incidents/02.Emptiness.html', '2. Emptiness'],
+      ['estate://incidents/03.Regrets.html', '3. Regrets'],
+      ['estate://incidents/04.TheBerkowitzManuevre.html', '4. The Berkowitz Manuevre'],
+      ['estate://incidents/05.Ignored.html', '5. Ignored'],
+      ['estate://incidents/06.Birthstones.html', '6. Birthstones'],
+      ['estate://incidents/07.Water.html', '7. Water'],
+      ['estate://incidents/08.Waking.html', '8. Waking'],
+      ['estate://incidents/09.TheMan.html', '9. The Man'],
+      ['estate://incidents/10.Ghosts.html', '10. Ghosts'],
+      ['estate://incidents/11.TheEnd.html', '11. The End'],
+      ['estate://incidents/12.TheIncident.html', '12. The Incident'],
+      ['estate://incidents/13.DuckTyping.html', '13. Duck Typing'],
+      ['estate://incidents/14.TheManWhoHappenedToReachUp.html', '14. The Man Who Happened to Reach Up'],
+      ['estate://incidents/15.TheManWhoHappenedToHaveLegs.html', '15. The Man Who Happened to Have Legs'],
+      ['estate://incidents/16.TheSkier.html', '16. The Skier'],
+      ['estate://incidents/17.JavekAndTheCandle.html', '17. Javek and the Candle'],
+      ['estate://incidents/18.MilkPowers.html', '18. Milk Powers'],
+      ['estate://incidents/19.TheLittlePieceOfCloth.html', '19. The Little Piece of Cloth'],
+      ['estate://incidents/20.TheTandemBicycle.html', '20. The Tandem Bicycle'],
+      ['estate://incidents/21.TheQueenSized.html', '21. The Queen-Sized'],
+      ['estate://incidents/22.Wristwatches.html', '22. Wristwatches'],
+      ['estate://incidents/23.TheAdvisor.html', '23. The Advisor'],
+      ['estate://incidents/24.SpeakingOfFlutes.html', '24. Speaking of Flutes'],
+      ['estate://incidents/25.KimothysMouth.html', '25. Kimothy\'s Mouth'],
+      ['estate://incidents/26.TheJumpWanter.html', '26. The Jump Wanter'],
+      ['estate://incidents/27.TheLifeGuy.html', '27. The Life Guy'],
+      ['estate://incidents/28.TheStoryLifeDoesntExplain.html', '28. The Story Life Doesn\'t Explain'],
+      ['estate://incidents/29.TheGrievingBoar.html', '29. The Grieving Boar'],
+      ['estate://incidents/30.JentleAndPailey.html', '30. Jentle and Pailey'],
+      ['estate://incidents/31.TheHandWhichFellApart.html', '31. The Hand Which Fell Apart'],
+      ['estate://incidents/32.ASmartCurtsy.html', '32. A Smart Curtsy'],
+      ['estate://incidents/33.TheSecretSandwich.html', '33. The Secret Sandwich'],
+      ['estate://incidents/34.AMagicMilk.html', '34. A Magic Milk'],
+      ['estate://incidents/35.AdventureOfTheApplesMom.html', '35. Adventure of the Apple\'s Mom']
+    ]],
+    ['Five-Minute Plays for Twins Who Don\'t Have Their Other Twin With Them and An Unlimited Supply of Animals', [
+      ['estate://five_minute_plays/withAnOctopus.html', 'Octopus'],
+      ['estate://five_minute_plays/withAnEagle.html', 'Eagle'],
+      ['estate://five_minute_plays/withAnSwan.html', 'Swan'],
+      ['estate://five_minute_plays/withAnOtter.html', 'Otter']
+    ]],
+    ['Quatrains', [
+      ['estate://quatrains/0.0.html', '0.0'],
+      ['estate://quatrains/0.1.html', '0.1'],
+      ['estate://quatrains/0.2.html', '0.2'],
+      ['estate://quatrains/1.0.html', '1.0'],
+      ['estate://quatrains/1.1.html', '1.1'],
+      ['estate://quatrains/1.2.html', '1.2'],
+      ['estate://quatrains/1.3.html', '1.3'],
+      ['estate://quatrains/1.4.html', '1.4']
+    ]],
+    ['Hacking', [
+      ['estate://hacking/aQuickGuideToSQLite.html', 'A Quick Guide to SQLite and Ruby'],
+      ['estate://hacking/rubyOneEightOh.html', 'What\'s Shiny and New in Ruby 1.8.0?'],
+      ['estate://hacking/seeingMetaclassesClearly.html', 'Seeing Metaclasses Clearly'],
+      ['estate://hacking/theFullyUpturnedBin.html', 'The Fully Upturned Bin'],
+      ['estate://hacking/theLittleCodersPredicament.html', 'The Little Coder\'s Predicament'],
+      ['estate://hacking/wearingRubySlippersToWork.html', 'Wearing Ruby Slippers To Work']
+    ]],
+    ['Twitter', [
+      ['http://favstar.fm/users/_why', '@_why\'s most favourited tweets'],
+      ['estate://twitter/lastTweets.html', '@_why\'s last ~300 tweets']
+    ]],
+    ['The Preeventualist', [
+      ['estate://preeventualist/', 'Mirror of the website, missing the FAQ page'],
+      ['http://ashraym.blogspot.com/', '"Ashley Raymond\'s" blog']
+    ]],
+    ['Soccer Riot', [
+      ['http://members.tripod.com/soccer_riot/', 'The website (still kicking!)'],
+      ['estate://SoccerRiot/', 'An ad-free mirror, hosted here (just in case)'],
+      ['Transcripts of some stories originally embedded in images', [
+        ['estate://soccer_riot/pickingUpTheDaddybird.html', 'Picking Up the Daddybird'],
+        ['estate://soccer_riot/iKnowFrenchKissingHasAHeadStartOnMe.html', 'I Know French Kissing has a Head Start On Me'],
+        ['estate://soccer_riot/theGoodCon.html', 'The Good Con']
+      ]]
+    ]],
+    ['Blogs', [
+      ['http://railsblob.blogspot.com/', 'My Rails Blob'],
+      ['http://onthelakebythesnacks.blogspot.com/', 'On the Lake by the Snacks']
+    ]],
+    ['http://runciter.net/potion/', 'Potion: A Short Pamphlet']
+  ]],
+  ['Pictures', [
+    ['Comics', [
+      ['Holy Bible', [
+        ['horizontal'],
+
+        ['estate://holy_bible/Holy-Bible-1.gif', 'one'],
+        ['estate://holy_bible/Holy-Bible-2.gif', 'two'],
+        ['estate://holy_bible/Holy-Bible-3.gif', 'three'],
+        ['estate://holy_bible/Holy-Bible-4.gif', 'four'],
+        ['estate://holy_bible/Holy-Bible-5.gif', 'five'],
+        ['estate://holy_bible/Holy-Bible-6.gif', 'six'],
+        ['estate://holy_bible/Holy-Bible-7.gif', 'seven'],
+        ['estate://holy_bible/Holy-Bible-8.gif', 'eight'],
+        ['estate://holy_bible/Holy-Bible-9.gif', 'nine'],
+        ['estate://holy_bible/Holy-Bible-10.gif', 'ten'],
+        ['estate://holy_bible/Holy-Bible-11.gif', 'eleven'],
+        ['estate://holy_bible/Holy-Bible-12.gif', 'twelve'],
+        ['estate://holy_bible/Holy-Bible-13.gif', 'thirteen'],
+        ['estate://holy_bible/Holy-Bible-14.png', 'fourteen'],
+        ['estate://holy_bible/Holy-Bible-15.png', 'fifteen'],
+        ['estate://holy_bible/Holy-Bible-16.png', 'sixteen']
+      ]],
+      ['The Open Window', [
+        ['horizontal'],
+
+        ['estate://the_open_window/The-Open-Window-1.jpg', 'one'],
+        ['estate://the_open_window/The-Open-Window-2.jpg', 'two'],
+        ['estate://the_open_window/The-Open-Window-3.jpg', 'three'],
+        ['estate://the_open_window/The-Open-Window-4.jpg', 'four'],
+        ['estate://the_open_window/The-Open-Window-5.jpg', 'five'],
+        ['estate://the_open_window/The-Open-Window-6.jpg', 'six'],
+        ['estate://the_open_window/The-Open-Window-7.jpg', 'seven'],
+        ['estate://the_open_window/The-Open-Window-8.jpg', 'eight'],
+        ['estate://the_open_window/The-Open-Window-9.jpg', 'nine']
+      ]],
+      ['Lost Children, Lost Children, Lost Children, See Such Feverish Castles', [
+        ['horizontal'],
+
+        ['estate://lost_children/lostChildrenLostChildrenLostChildrenSeeSuchFeverishCastles.html', 'one']
+      ]],
+      ['The Least Surprised (some missing)', [
+        ['horizontal'],
+
+        ['estate://the_least_surprised/the-least-suprised-1.png', 'one'],
+        ['estate://the_least_surprised/the-least-suprised-3.png', 'three'],
+        ['estate://the_least_surprised/the-least-suprised-4.png', 'four'],
+        ['estate://the_least_surprised/the-least-suprised-9.png', 'nine'],
+        ['estate://the_least_surprised/the-least-suprised-12.png', 'twelve'],
+        ['estate://the_least_surprised/the-least-suprised-13.png', 'thirteen'],
+        ['estate://the_least_surprised/the-least-suprised-14.png', 'fourteen']
+      ]]
+    ]],
+    ['RubyKaigi Wallpapers', [
+      ['http://jp.rubyist.net/RubyKaigi2007/Goodies.html', '2007'],
+      ['http://jp.rubyist.net/RubyKaigi2008/?Goodies', '2008']
+    ]]
+  ]],
+  ['Music', [
+    ['The Thirsty Cups', [
+      ['http://mislav.uniqpath.com/poignant-guide/soundtrack/', 'The Poignant Guide soundtrack'],
+      ['http://www.archive.org/details/whytheluckystiff2006-06-23.railsconf_chicago', 'RailsConf Chicago 23 June 2006']
+    ]],
+    ['The Child Who Was a Keyhole', [
+      ['http://www.myspace.com/thechildwhowasakeyhole', 'Band MySpace'],
+      ['http://www.cdbaby.com/cd/tcwwak', 'In The Faxed Atmosphere']
+    ]],
+    ['moonboots', [
+      ['http://ftp.df.lth.se/pub/scene.org/music/groups/starving_but_happy/sbh-lp003-moonboots-elfin_princess_(starvingbuthappy,_2004)/', 'elfin princess']
+    ]]
+  ]],
+  ['Videos', [
+    ['The Least Surprised', [
+      ['http://www.youtube.com/watch?v=ng95M-cRb44', 'Time.now.is_a? MagicTime'],
+      ['http://www.youtube.com/watch?v=mXZ_htL1rOl', 'The Red Scared'],
+      ['http://www.youtube.com/watch?v=Du_RTMmofWM', 'UnboundMyth\'d']
+    ]],
+    ['OSCON 2005', [
+      ['http://www.youtube.com/watch?v=lwDDa9ctNFE', 'Foxes Tall and Small on Modules'],
+      ['http://www.youtube.com/watch?v=mKSacTAjS0Q', 'Sinking Symphony']
+    ]],
+    ['Noboto', [
+      ['http://noboto.com/15/noboto-12', 'Episode 12: Why'],
+      ['http://noboto.com/16/noboto-13', 'Episode 13: Poignant']
+    ]],
+    ['Art && Code Symposium', [
+      ['http://www.vimeo.com/5047563', 'Hackety Hack'],
+      ['http://www.youtube.com/watch?v=l4U1vO-lMS0', 'Just the Kaxxt portion of the above video']
+    ]],
+    ['RailsConf 2006', [
+      ['http://video.google.com/videoplay?docid=7470881644241559852', 'Why the Lucky Stiff presents at RailsConf 2006'],
+      ['http://video.google.com/videoplay?docid=5517353410373048287', 'David Heinemeier Hansson and Why the Lucky Stiff chat at RailsConf 2006'],
+      ['http://everyoneishereinthefuture.com/', 'Everyone Is Here In The Future']
+    ]]
+  ]],
+  ['Links', [
+    ['Archived websites and blogs', [
+      ['http://web.archive.org/web/*/http://whytheluckystiff.net', 'whytheluckystiff.net'],
+      ['http://web.archive.org/web/*/whytheluckystiff.net/quiet/', '(.~) what a quiet stiff (~.)'],
+      ['http://web.archive.org/web/*/http://hackety.org', 'hackety.org'],
+      ['http://web.archive.org/web/*/http://redhanded.hobix.com', 'redhanded.hobix.com'],
+      ['http://web.archive.org/web/*/http://hobix.com', 'hobix.com'],
+      ['http://web.archive.org/web/*/http://shoooes.net', 'shoooes.net'],
+      ['http://web.archive.org/web/*/http://preeventualist.org', 'preeventualist.org']
+    ]],
+    ['Dwellings left standing', [
+      ['http://www.advogato.org/person/whytheluckystiff/', 'Advogato Blog'],
+      ['http://www.flickr.com/photos/_why', 'Flickr Account'],
+      ['http://delicious.com/why', 'why\'s Bookmarks on Delicious']
+    ]],
+    ['Lucky stiffs', [
+      ['http://ejohn.org/blog/eulogy-to-_why/', 'Eulogy to _why'],
+      ['http://ism.creativepony.com/post/169621989/the-physics-of-a-lucky-stiff', 'The Physics of a Lucky Stiff'],
+      ['http://shack-a-nerd.livejournal.com/70594.html', 'Disappearance']
+    ]],
+    ['http://rubyforge.org/pipermail/poignant-stiffs/', 'The poignant-stiffs Archives'],
+    ['http://_why.usesthis.com/', '_why interview by The Setup'],
+    ['http://www.reddit.com/r/programming/comments/9c5on/where_is_why/c0c83zg', 'An email']
+  ]]
+]
+
+NOTES = {
+  'Incidents' => '_why\'s <strong>incidents</strong> are inspired by <a href="http://www.reocities.com/Athens/8926/Kharms/Incidences.html">Daniil Kharms\' Incidences</a>. Kharms is a large influence on _why: <a href="http://rubyforge.org/pipermail/poignant-stiffs/2005-March.txt">"Gogol and Kafka and Kharms (and anything Oberiu) hold the lion\'s share in my mind\'s governing committee."</a>',
+  'The Preeventualist' => '_why created <strong>The Preeventualist</strong> to go along with his Poignant Guide. It\'s a philosophy he made up for the book. It comes up in <a href="http://mislav.uniqpath.com/poignant-guide/book/chapter-6.html">chapter 6</a>, right at the beginning. Read <a href="http://shack-a-nerd.livejournal.com/70594.html">shack_a_nerd\'s story</a> about this, you\'ll love it!!',
+  'Soccer Riot' => '<strong>Soccer Riot Magazine</strong> appears to be _why\'s first website. I hope he doesn\'t mind me drawing attention to it. I\'m not sure what he thinks of these old writings and I am worried that he doesn\'t want them elevated like this. But I will elevate them nonetheless, as I find them just as valuable as any of his other works. Still, I purposely didn\'t provide any links to the various well-hidden pages on the site. There\'s a lot of flash and the site is choppy and unorganized. So you\'ll have to blaze through it yourself, as _why intended.',
+  'Blogs' => '<strong>My Rails Blob</strong> is a satirical Ruby on Rails blog by _why. <strong>On the Lake by the Snacks</strong> is part of the <a href="http://www.turbulence.org/Works/ten-sided/about.html">Ten-sided</a> project, for which _why is listed as a writer.',
+  'moonboots' => 'It can be confirmed that <strong>moonboots</strong> is some sort of band _why was in. By putting this on the list I\'m starting to creep past the persona of _why to the real person lying behind it, and that\'s exactly what I don\'t want to do on this page. I just decided to include this for those people who desire more of _why\'s unique music.',
+  '(.~) what a quiet stiff (~.)' => '<strong>(.~) what a quiet stiff (~.)</strong> was a photoblog by _why. He talks about it in one of his <a href="http://www.advogato.org/person/whytheluckystiff/diary/48.html">Advogato entries</a>.'
+}
+
+INDEX_FOOTER = '<hr /><p style="font-size: 10pt;"><img src="elf.png" alt="" title="A crucial remark from the Elf.  The ham undoubtedly concurs." /> This page is totally mutable! Just send in to <a href="mailto:jeremy.ruten@gmail.com">jeremy.ruten@gmail.com</a> with your rousing list of ideas.</p>'
+
