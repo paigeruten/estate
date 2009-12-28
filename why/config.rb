1611407b631c738a95eb3577b67ce6d0f43502b1
Jeremy Ruten <jeremy.ruten@gmail.com>
2009-12-27 21:26:40 -0600

    updated link to why.zip

--- why/config.rb
+++ why/config.rb
@@ -1,4 +1,4 @@
 TITLE = '_why\'s Estate'
 
-DESCRIPTION = 'Hey, welcome to my collection of <a href="http://wikipedia.org/wiki/Why_the_lucky_stiff">why the lucky stiff</a> links.  Everything _why has published on the internet should be accessible from here.  It works sort of like a museum that sells maps.  Many of his abandoned writings are mirrored locally here, and everything else is through external links.  To download the Estate perhaps for local viewing, get <a href="http://cloud.github.com/downloads/jeremyr/estate/why.zip">why.zip</a> (5.4 MB).'
+DESCRIPTION = 'Hey, welcome to my collection of <a href="http://wikipedia.org/wiki/Why_the_lucky_stiff">why the lucky stiff</a> links.  Everything _why has published on the internet should be accessible from here.  It works sort of like a museum that sells maps.  Many of his abandoned writings are mirrored locally here, and everything else is through external links.  To download the Estate perhaps for local viewing, get <a href="http://cloud.github.com/downloads/yjerem/estate/why.zip">why.zip</a> (5.4 MB).'
 
