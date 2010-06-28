7a63643a25c21a9d2ff10ba16c426d9be9c20020
Jeremy Ruten <jeremy.ruten@gmail.com>
2010-06-28 00:53:08 -0600

    whoops, left in a link to whyday. wasn't sure whether there should be such a link. i don't think i'll link to news items or anything current like that. it's a static archive.

--- why/config.rb
+++ why/config.rb
@@ -1,6 +1,4 @@
 TITLE = '_why\'s Estate'
 
-DESCRIPTION = 'Hey, welcome to my collection of <a href="http://wikipedia.org/wiki/Why_the_lucky_stiff">why the lucky stiff</a> links.  Everything _why has published on the internet should be accessible from here.  It works sort of like a museum that sells maps.  Many of his abandoned writings are mirrored locally here, and everything else is through external links.  To download the Estate perhaps for local viewing, get <a href="http://cloud.github.com/downloads/yjerem/estate/why-2010-06-28.zip">why.zip</a> (5.4 MB).
-<br /><br />
-<a href="http://whyday.org/">Celebrate Whyday on August 19</a>'
+DESCRIPTION = 'Hey, welcome to my collection of <a href="http://wikipedia.org/wiki/Why_the_lucky_stiff">why the lucky stiff</a> links.  Everything _why has published on the internet should be accessible from here.  It works sort of like a museum that sells maps.  Many of his abandoned writings are mirrored locally here, and everything else is through external links.  To download the Estate perhaps for local viewing, get <a href="http://cloud.github.com/downloads/yjerem/estate/why-2010-06-28.zip">why.zip</a> (5.8 MB).'
 
