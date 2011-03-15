e4136fae64e26ca43ac101f93afc8ee02850091a
Jeremy Ruten <jeremy.ruten@gmail.com>
2011-03-14 19:08:23 -0600

    Link to "A Costly Parade" and "This Hack Was Not Properly Planned" mirrors

--- why/index.rb
+++ why/index.rb
@@ -128,6 +128,11 @@ LIST = [
     ]],
     ['Blogs', [
       ['http://viewsourcecode.org/why/redhanded/', 'RedHanded (mirror)'],
+      ['Hackety', [
+        ['http://web.archive.org/web/*/http://hackety.org', 'Old archive of hackety.org'],
+        ['http://lubutu.com/mirror/a-costly-parade', 'A Costly Parade'],
+        ['http://lubutu.com/mirror/this-hack-was-not-properly-planned', 'This Hack Was Not Properly Planned']
+      ]],
       ['http://railsblob.blogspot.com/', 'My Rails Blob'],
       ['http://onthelakebythesnacks.blogspot.com/', 'On the Lake by the Snacks']
     ]],
