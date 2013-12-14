f7f9a24a6f319ddc11368e5636decc1ca3a8e695
Jeremy Ruten <jeremy.ruten@gmail.com>
2013-12-14 12:34:26 -0600

    Remove archive.org notice

--- why/config.rb
+++ why/config.rb
@@ -1,8 +1,6 @@
 TITLE = '_why\'s Estate'
 
 DESCRIPTION = <<html
-Hey, welcome to my collection of <a href="http://wikipedia.org/wiki/Why_the_lucky_stiff">why the lucky stiff</a> links.  Everything _why has published on the internet should be accessible from here.  It works sort of like a museum that sells maps.  Many of his abandoned writings are mirrored locally here, and everything else is through external links.  To download the Estate perhaps for local viewing, get <a href="http://cloud.github.com/downloads/yjerem/estate/why-2010-06-28.zip">why.zip</a> (5.8 MB).</p>
-
-<p><strong>2013-11-07:</strong> <a href="https://www.crowdtilt.com/campaigns/please-help-the-internet-archive-after-their-offices-were-destroyed-in-a-fire">Help the Internet Archive - their offices were destroyed in a fire</a>. Without archive.org, most of _why's writings would have been lost forever, and _why's Estate would be vastly truncated! Please consider helping them out. :)
+Hey, welcome to my collection of <a href="http://wikipedia.org/wiki/Why_the_lucky_stiff">why the lucky stiff</a> links.  Everything _why has published on the internet should be accessible from here.  It works sort of like a museum that sells maps.  Many of his abandoned writings are mirrored locally here, and everything else is through external links.  To download the Estate perhaps for local viewing, get <a href="http://cloud.github.com/downloads/yjerem/estate/why-2010-06-28.zip">why.zip</a> (5.8 MB).
 html
 
